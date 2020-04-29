a2cec53615ef37402c32c23c571ce82c26682bb7
Nikita Marchenko <nikita.marchenko@gmail.com>
2020-04-29 14:00:11 +0300

    fix tests

--- src-java/server42/server42-control/src/test/java/org/openkilda/server42/control/kafka/GateTest.java
+++ src-java/server42/server42-control/src/test/java/org/openkilda/server42/control/kafka/GateTest.java
@@ -1,5 +1,19 @@
-package org.openkilda.server42.control.kafka;
+/* Copyright 2020 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
+package org.openkilda.server42.control.kafka;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.argThat;
@@ -149,7 +163,6 @@ public class GateTest {
 
     @Test
     public void listFlowsTest() throws Exception {
-        Headers headers = Headers.builder().correlationId("some-correlation-id").build();
 
         Builder commandPacketResponseBuilded = CommandPacketResponse.newBuilder();
 
@@ -161,8 +174,11 @@ public class GateTest {
 
         CommandPacketResponse commandPacketResponse = commandPacketResponseBuilded.build();
 
-        when(zeroMqClient.send(argThat(commandPacket -> commandPacket.getType() == Type.LIST_FLOWS))).thenReturn(commandPacketResponse);
+        when(zeroMqClient.send(argThat(
+                commandPacket -> commandPacket.getType() == Type.LIST_FLOWS)))
+                .thenReturn(commandPacketResponse);
 
+        Headers headers = Headers.builder().correlationId("some-correlation-id").build();
         gate.listen(new ListFlowsRequest(headers));
 
         ArgumentCaptor<ListFlowsResponse> argument = ArgumentCaptor.forClass(ListFlowsResponse.class);
