jonvestal/open-kilda

Zero downtime floodlight router

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/ControllerToSpeakerProxyBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.AbstractMessage;
 import org.openkilda.messaging.Message;
 import org.openkilda.messaging.command.CommandData;
@@ -23,6 +24,10 @@ import org.openkilda.messaging.command.stats.StatsRequest;
 import org.openkilda.model.SwitchId;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
+import org.openkilda.wfm.topology.floodlightrouter.ComponentType;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMapping;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMappingUpdate;
@@ -59,9 +64,12 @@ public class ControllerToSpeakerProxyBolt extends AbstractBolt implements Contro
 
     @Override
     protected void dispatch(Tuple input) throws Exception {
-        if (SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
+        if (ComponentType.ZK_SPOUT.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (active && SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
             handleSwitchMappingUpdate(input);
-        } else {
+        } else if (active) {
             super.dispatch(input);
         }
     }
@@ -137,6 +145,8 @@ public class ControllerToSpeakerProxyBolt extends AbstractBolt implements Contro
                 FieldNameBasedTupleToKafkaMapper.BOLT_KEY, FieldNameBasedTupleToKafkaMapper.BOLT_MESSAGE,
                 RegionAwareKafkaTopicSelector.FIELD_ID_TOPIC, RegionAwareKafkaTopicSelector.FIELD_ID_REGION);
         outputFieldsDeclarer.declare(fields);
+        outputFieldsDeclarer.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 
     protected Object pullControllerPayload(Tuple tuple) throws PipelineException {

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/RegionTrackerBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.AliveRequest;
 import org.openkilda.messaging.AliveResponse;
 import org.openkilda.messaging.Message;
@@ -26,6 +27,9 @@ import org.openkilda.persistence.repositories.FeatureTogglesRepository;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
 import org.openkilda.wfm.share.bolt.MonotonicClock;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
 import org.openkilda.wfm.topology.floodlightrouter.ComponentType;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.Stream;
@@ -87,11 +91,14 @@ public class RegionTrackerBolt extends AbstractBolt implements RegionMonitorCarr
     @Override
     protected void dispatch(Tuple input) throws Exception {
         String source = input.getSourceComponent();
-        if (monotonicTickMatch.isTick(input)) {
+        if (ComponentType.ZK_SPOUT.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (active && monotonicTickMatch.isTick(input)) {
             handleTick();
-        } else if (SpeakerToNetworkProxyBolt.BOLT_ID.equals(source)) {
+        } else if (active && SpeakerToNetworkProxyBolt.BOLT_ID.equals(source)) {
             handleNetworkNotification(input);
-        } else {
+        } else if (active) {
             super.dispatch(input);
         }
     }
@@ -109,6 +116,8 @@ public class RegionTrackerBolt extends AbstractBolt implements RegionMonitorCarr
         outputManager.declareStream(STREAM_SPEAKER_ID, fields);
 
         outputManager.declareStream(STREAM_REGION_NOTIFICATION_ID, STREAM_REGION_NOTIFICATION_FIELDS);
+        outputManager.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 
     @Override
@@ -151,7 +160,7 @@ public class RegionTrackerBolt extends AbstractBolt implements RegionMonitorCarr
     private void handleRegionNotification(Tuple input) throws PipelineException {
         String region = pullSpeakerRegion(input);
         InfoData payload = pullValue(input, SpeakerToNetworkProxyBolt.FIELD_ID_PAYLOAD, InfoData.class);
-        if (! handleRegionNotification(region, payload)) {
+        if (!handleRegionNotification(region, payload)) {
             unhandledInput(input);
         }
     }

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/SpeakerToControllerProxyBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.info.InfoData;
 import org.openkilda.messaging.info.InfoMessage;
 import org.openkilda.messaging.info.event.ConnectedDevicePacketBase;
@@ -25,6 +26,10 @@ import org.openkilda.messaging.info.event.PortInfoData;
 import org.openkilda.model.SwitchId;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
+import org.openkilda.wfm.topology.floodlightrouter.ComponentType;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMapping;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMappingUpdate;
@@ -55,9 +60,12 @@ public class SpeakerToControllerProxyBolt extends AbstractBolt {
 
     @Override
     protected void dispatch(Tuple input) throws Exception {
-        if (SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
+        if (ComponentType.ZK_SPOUT.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (active && SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
             handleSwitchMappingUpdate(input);
-        } else {
+        } else if (active) {
             super.dispatch(input);
         }
     }
@@ -150,6 +158,8 @@ public class SpeakerToControllerProxyBolt extends AbstractBolt {
                 FieldNameBasedTupleToKafkaMapper.BOLT_KEY, FieldNameBasedTupleToKafkaMapper.BOLT_MESSAGE,
                 RegionAwareKafkaTopicSelector.FIELD_ID_TOPIC, RegionAwareKafkaTopicSelector.FIELD_ID_REGION);
         outputFieldsDeclarer.declare(fields);
+        outputFieldsDeclarer.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 
     protected Values makeDefaultTuple(String key, Object payload) {

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/SwitchMonitorBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.Message;
 import org.openkilda.messaging.info.InfoData;
 import org.openkilda.messaging.info.InfoMessage;
@@ -23,6 +24,9 @@ import org.openkilda.messaging.info.event.SwitchInfoData;
 import org.openkilda.model.SwitchId;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
 import org.openkilda.wfm.topology.floodlightrouter.ComponentType;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMappingUpdate;
@@ -59,13 +63,16 @@ public class SwitchMonitorBolt extends AbstractBolt implements SwitchMonitorCarr
     @Override
     protected void handleInput(Tuple input) throws PipelineException {
         String source = input.getSourceComponent();
-        if (MonotonicTick.BOLT_ID.equals(source)) {
+        if (ComponentType.ZK_SPOUT.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (MonotonicTick.BOLT_ID.equals(source) && active) {
             service.handleTimerTick();
-        } else if (RegionTrackerBolt.BOLT_ID.equals(source)) {
+        } else if (RegionTrackerBolt.BOLT_ID.equals(source) && active) {
             handleRegionOfflineNotification(input);
-        } else if (SpeakerToNetworkProxyBolt.BOLT_ID.equals(source)) {
+        } else if (SpeakerToNetworkProxyBolt.BOLT_ID.equals(source) && active) {
             handleSwitchConnectionNotification(input);
-        } else {
+        } else if (active) {
             unhandledInput(input);
         }
     }
@@ -132,5 +139,7 @@ public class SwitchMonitorBolt extends AbstractBolt implements SwitchMonitorCarr
         streamManager.declareStream(STREAM_NETWORK_ID, kafkaProducerFields);
 
         streamManager.declareStream(STREAM_REGION_MAPPING_ID, STREAM_REGION_MAPPING_FIELDS);
+        streamManager.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 }

telstra/Kilda

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/ControllerToSpeakerProxyBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.AbstractMessage;
 import org.openkilda.messaging.Message;
 import org.openkilda.messaging.command.CommandData;
@@ -23,6 +24,9 @@ import org.openkilda.messaging.command.stats.StatsRequest;
 import org.openkilda.model.SwitchId;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMapping;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMappingUpdate;
@@ -59,9 +63,12 @@ public class ControllerToSpeakerProxyBolt extends AbstractBolt implements Contro
 
     @Override
     protected void dispatch(Tuple input) throws Exception {
-        if (SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
+        if (ZooKeeperSpout.BOLT_ID.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (active && SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
             handleSwitchMappingUpdate(input);
-        } else {
+        } else if (active) {
             super.dispatch(input);
         }
     }
@@ -137,6 +144,8 @@ public class ControllerToSpeakerProxyBolt extends AbstractBolt implements Contro
                 FieldNameBasedTupleToKafkaMapper.BOLT_KEY, FieldNameBasedTupleToKafkaMapper.BOLT_MESSAGE,
                 RegionAwareKafkaTopicSelector.FIELD_ID_TOPIC, RegionAwareKafkaTopicSelector.FIELD_ID_REGION);
         outputFieldsDeclarer.declare(fields);
+        outputFieldsDeclarer.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 
     protected Object pullControllerPayload(Tuple tuple) throws PipelineException {

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/RegionTrackerBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.AliveRequest;
 import org.openkilda.messaging.AliveResponse;
 import org.openkilda.messaging.Message;
@@ -26,6 +27,9 @@ import org.openkilda.persistence.repositories.FeatureTogglesRepository;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
 import org.openkilda.wfm.share.bolt.MonotonicClock;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
 import org.openkilda.wfm.topology.floodlightrouter.ComponentType;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.Stream;
@@ -87,11 +91,14 @@ public class RegionTrackerBolt extends AbstractBolt implements RegionMonitorCarr
     @Override
     protected void dispatch(Tuple input) throws Exception {
         String source = input.getSourceComponent();
-        if (monotonicTickMatch.isTick(input)) {
+        if (ZooKeeperSpout.BOLT_ID.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (active && monotonicTickMatch.isTick(input)) {
             handleTick();
-        } else if (SpeakerToNetworkProxyBolt.BOLT_ID.equals(source)) {
+        } else if (active && SpeakerToNetworkProxyBolt.BOLT_ID.equals(source)) {
             handleNetworkNotification(input);
-        } else {
+        } else if (active) {
             super.dispatch(input);
         }
     }
@@ -109,6 +116,8 @@ public class RegionTrackerBolt extends AbstractBolt implements RegionMonitorCarr
         outputManager.declareStream(STREAM_SPEAKER_ID, fields);
 
         outputManager.declareStream(STREAM_REGION_NOTIFICATION_ID, STREAM_REGION_NOTIFICATION_FIELDS);
+        outputManager.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 
     @Override
@@ -151,7 +160,7 @@ public class RegionTrackerBolt extends AbstractBolt implements RegionMonitorCarr
     private void handleRegionNotification(Tuple input) throws PipelineException {
         String region = pullSpeakerRegion(input);
         InfoData payload = pullValue(input, SpeakerToNetworkProxyBolt.FIELD_ID_PAYLOAD, InfoData.class);
-        if (! handleRegionNotification(region, payload)) {
+        if (!handleRegionNotification(region, payload)) {
             unhandledInput(input);
         }
     }

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/SpeakerToControllerProxyBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.info.InfoData;
 import org.openkilda.messaging.info.InfoMessage;
 import org.openkilda.messaging.info.event.ConnectedDevicePacketBase;
@@ -25,6 +26,9 @@ import org.openkilda.messaging.info.event.PortInfoData;
 import org.openkilda.model.SwitchId;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMapping;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMappingUpdate;
@@ -55,9 +59,12 @@ public class SpeakerToControllerProxyBolt extends AbstractBolt {
 
     @Override
     protected void dispatch(Tuple input) throws Exception {
-        if (SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
+        if (ZooKeeperSpout.BOLT_ID.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (active && SwitchMonitorBolt.BOLT_ID.equals(input.getSourceComponent())) {
             handleSwitchMappingUpdate(input);
-        } else {
+        } else if (active) {
             super.dispatch(input);
         }
     }
@@ -150,6 +157,8 @@ public class SpeakerToControllerProxyBolt extends AbstractBolt {
                 FieldNameBasedTupleToKafkaMapper.BOLT_KEY, FieldNameBasedTupleToKafkaMapper.BOLT_MESSAGE,
                 RegionAwareKafkaTopicSelector.FIELD_ID_TOPIC, RegionAwareKafkaTopicSelector.FIELD_ID_REGION);
         outputFieldsDeclarer.declare(fields);
+        outputFieldsDeclarer.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 
     protected Values makeDefaultTuple(String key, Object payload) {

src-java/floodlightrouter-topology/floodlightrouter-storm-topology/src/main/java/org/openkilda/wfm/topology/floodlightrouter/bolts/SwitchMonitorBolt.java
@@ -15,6 +15,7 @@
 
 package org.openkilda.wfm.topology.floodlightrouter.bolts;
 
+import org.openkilda.bluegreen.LifecycleEvent;
 import org.openkilda.messaging.Message;
 import org.openkilda.messaging.info.InfoData;
 import org.openkilda.messaging.info.InfoMessage;
@@ -23,6 +24,9 @@ import org.openkilda.messaging.info.event.SwitchInfoData;
 import org.openkilda.model.SwitchId;
 import org.openkilda.wfm.AbstractBolt;
 import org.openkilda.wfm.error.PipelineException;
+import org.openkilda.wfm.share.zk.ZkStreams;
+import org.openkilda.wfm.share.zk.ZooKeeperBolt;
+import org.openkilda.wfm.share.zk.ZooKeeperSpout;
 import org.openkilda.wfm.topology.floodlightrouter.ComponentType;
 import org.openkilda.wfm.topology.floodlightrouter.RegionAwareKafkaTopicSelector;
 import org.openkilda.wfm.topology.floodlightrouter.model.RegionMappingUpdate;
@@ -59,13 +63,16 @@ public class SwitchMonitorBolt extends AbstractBolt implements SwitchMonitorCarr
     @Override
     protected void handleInput(Tuple input) throws PipelineException {
         String source = input.getSourceComponent();
-        if (MonotonicTick.BOLT_ID.equals(source)) {
+        if (ZooKeeperSpout.BOLT_ID.equals(input.getSourceComponent())) {
+            LifecycleEvent event = (LifecycleEvent) input.getValueByField(ZooKeeperSpout.FIELD_ID_LIFECYCLE_EVENT);
+            handleLifeCycleEvent(event);
+        } else if (MonotonicTick.BOLT_ID.equals(source) && active) {
             service.handleTimerTick();
-        } else if (RegionTrackerBolt.BOLT_ID.equals(source)) {
+        } else if (RegionTrackerBolt.BOLT_ID.equals(source) && active) {
             handleRegionOfflineNotification(input);
-        } else if (SpeakerToNetworkProxyBolt.BOLT_ID.equals(source)) {
+        } else if (SpeakerToNetworkProxyBolt.BOLT_ID.equals(source) && active) {
             handleSwitchConnectionNotification(input);
-        } else {
+        } else if (active) {
             unhandledInput(input);
         }
     }
@@ -132,5 +139,7 @@ public class SwitchMonitorBolt extends AbstractBolt implements SwitchMonitorCarr
         streamManager.declareStream(STREAM_NETWORK_ID, kafkaProducerFields);
 
         streamManager.declareStream(STREAM_REGION_MAPPING_ID, STREAM_REGION_MAPPING_FIELDS);
+        streamManager.declareStream(ZkStreams.ZK.toString(), new Fields(ZooKeeperBolt.FIELD_ID_STATE,
+                ZooKeeperBolt.FIELD_ID_CONTEXT));
     }
 }