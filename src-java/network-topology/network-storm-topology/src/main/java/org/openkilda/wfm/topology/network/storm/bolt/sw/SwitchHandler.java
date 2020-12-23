surabujin/open-kilda

Workaround for loss of switch disconnect event
Reimplementation loss of switch disconnect event handling to avoid
unintended offline of all switches (and down of all flows) on network
topology replug.

src-java/network-topology/network-storm-topology/src/main/java/org/openkilda/wfm/topology/network/storm/bolt/sw/SwitchHandler.java
@@ -250,8 +250,8 @@ public class SwitchHandler extends AbstractBolt implements ISwitchCarrier {
         service.switchBecomeUnmanaged(datapath);
     }
 
-    public void processSwitchBecomeManaged(SpeakerSwitchView switchView) {
-        service.switchBecomeManaged(switchView);
+    public void processSwitchBecomeManaged(SpeakerSwitchView switchView, String correlationId) {
+        service.switchBecomeManaged(switchView, correlationId);
     }
 
     public void processSwitchAddWithHistory(HistoryFacts history) {

telstra/Kilda

src-java/network-topology/network-storm-topology/src/main/java/org/openkilda/wfm/topology/network/storm/bolt/sw/SwitchHandler.java
@@ -250,8 +250,8 @@ public class SwitchHandler extends AbstractBolt implements ISwitchCarrier {
         service.switchBecomeUnmanaged(datapath);
     }
 
-    public void processSwitchBecomeManaged(SpeakerSwitchView switchView) {
-        service.switchBecomeManaged(switchView);
+    public void processSwitchBecomeManaged(SpeakerSwitchView switchView, String dumpId) {
+        service.switchBecomeManaged(switchView, dumpId);
     }
 
     public void processSwitchAddWithHistory(HistoryFacts history) {