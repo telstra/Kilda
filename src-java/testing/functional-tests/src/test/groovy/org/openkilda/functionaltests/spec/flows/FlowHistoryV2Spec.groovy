e3d4482af08be33f70ea87be392b925c060964bb
Andrii Dovhan <dovhan.andriy@gmail.com>
2020-12-30 15:20:42 +0200

    add test for #3938

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/FlowHistoryV2Spec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/FlowHistoryV2Spec.groovy
@@ -1,16 +1,25 @@
 package org.openkilda.functionaltests.spec.flows
 
+import static org.openkilda.functionaltests.extension.tags.Tag.LOW_PRIORITY
 import static org.openkilda.functionaltests.extension.tags.Tag.SMOKE
+import static org.openkilda.functionaltests.extension.tags.Tag.VIRTUAL
 import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.CREATE_ACTION
 import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.CREATE_SUCCESS
+import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.REROUTE_ACTION
+import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.REROUTE_FAIL
 import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.UPDATE_ACTION
 import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.UPDATE_SUCCESS
 import static org.openkilda.testing.Constants.NON_EXISTENT_FLOW_ID
+import static org.openkilda.testing.Constants.WAIT_OFFSET
+import static org.openkilda.testing.service.floodlight.model.FloodlightConnectMode.RW
 
 import org.openkilda.functionaltests.HealthCheckSpecification
 import org.openkilda.functionaltests.extension.failfast.Tidy
 import org.openkilda.functionaltests.extension.tags.Tags
+import org.openkilda.functionaltests.helpers.Wrappers
 import org.openkilda.messaging.error.MessageError
+import org.openkilda.messaging.info.event.IslChangeType
+import org.openkilda.messaging.payload.flow.FlowState
 import org.openkilda.messaging.payload.history.FlowHistoryEntry
 import org.openkilda.model.FlowEncapsulationType
 import org.openkilda.model.PathComputationStrategy
@@ -296,6 +305,46 @@ class FlowHistoryV2Spec extends HealthCheckSpecification {
                         expectedHistory: []
                 ]
         ]
+    }
+
+    @Tidy
+    @Tags([VIRTUAL, LOW_PRIORITY])
+    //it doesn't work on the hardware env due to the 'round trip latency' feature
+    def "Root cause is registered in flow history while rerouting"() {
+        given: "An active flow"
+        def (Switch srcSwitch, Switch dstSwitch) = topology.activeSwitches
+        def flow = flowHelperV2.randomFlow(srcSwitch, dstSwitch)
+        flowHelperV2.addFlow(flow)
+
+        when: "Deactivate the src switch"
+        def blockData = switchHelper.knockoutSwitch(srcSwitch, RW)
+        def swIsActive = false
+
+        and: "Related ISLs are FAILED"
+        def isls = topology.getRelatedIsls(srcSwitch)
+        Wrappers.wait(discoveryTimeout + WAIT_OFFSET / 2) {
+            def allIsls = northbound.getAllLinks()
+            isls.each { assert islUtils.getIslInfo(allIsls, it).get().actualState == IslChangeType.FAILED }
+        }
+
+        then: "Flow goes DOWN"
+        Wrappers.wait(WAIT_OFFSET) {
+            def flowInfo = northboundV2.getFlow(flow.flowId)
+            assert flowInfo.status == FlowState.DOWN.toString()
+            assert flowInfo.statusInfo == "ValidateFlowAction failed: Flow's $flow.flowId src switch is not active"
+        }
+
+        and: "The root cause('Switch is not active') is registered in flow history"
+        def flowHistory = northbound.getFlowHistory(flow.flowId).find { it.action == REROUTE_ACTION }
+        assert flowHistory.payload[0].action == "Started flow validation"
+        assert flowHistory.payload[1].action == "ValidateFlowAction failed: Flow's $flow.flowId src switch is not active"
+        assert flowHistory.payload[2].action == REROUTE_FAIL
+
+        cleanup:
+        if (!swIsActive) {
+            switchHelper.reviveSwitch(srcSwitch, blockData, true)
+        }
+        flow && flowHelperV2.deleteFlow(flow.flowId)
     }
 
     void checkHistoryCreateV2Action(FlowHistoryEntry flowHistory, String flowId) {
